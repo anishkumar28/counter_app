87839add90f7ef260ccaf8c1b7aeb28d34d4b028
Anish Kumar <anishkumar6527@gmail.com>
2024-01-19 20:19:24 +0530

    counter func using useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  let [counter,setCounter] = useState(0);
+
+  const increaseValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    console.log('clicked', counter)
+  }
+
+  const decreaseValue = () => {
+    setCounter(counter - 1);
+ 
+  }
+
   return (
     <div className="App">
+      <h1>Learning React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={increaseValue}>Increase value {counter}</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease value {counter}</button>
+      <p>footer: {counter}</p>
     </div>
   );
 }
